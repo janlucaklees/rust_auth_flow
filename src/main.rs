b3fe54e409fff1023ed588b6306f7eb9098caf62
Jan-Luca Klees <janluca@leanatic.com>
2021-01-09 11:41:27 +0100

    Added Tera templating

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use tera::Tera;
 
 #[get("/")]
-async fn index() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+async fn index(tmpl: web::Data<tera::Tera>) -> impl Responder {
+    HttpResponse::Ok().content_type("text/html").body(
+        tmpl.render("index.html", &tera::Context::new()).unwrap()
+    )
 }
 
 #[post("/")]
@@ -14,9 +17,20 @@ async fn index_post(req_body: String) -> impl Responder {
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
+
+        let tera = match Tera::new("templates/**/*.html") {
+            Ok(t) => t,
+            Err(e) => {
+                println!("Parsing error(s): {}", e);
+                ::std::process::exit(1);
+            }
+        };
+
         App::new()
+            .data(tera)
             .service(index)
             .service(index_post)
+
     })
     .bind("127.0.0.1:8080")?
     .run()
