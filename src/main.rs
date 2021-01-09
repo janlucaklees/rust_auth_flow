90aad937c3fde5ceffcfbcfdebaa5049083802b3
Jan-Luca Klees <janluca@leanatic.com>
2021-01-09 12:19:35 +0100

    Login with redirect

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,49 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, post, web, http, App, HttpResponse, HttpServer, Responder};
 use tera::Tera;
+use serde::Deserialize;
+
 
 #[get("/")]
-async fn index(tmpl: web::Data<tera::Tera>) -> impl Responder {
+async fn index(
+    tmpl: web::Data<tera::Tera>
+) -> impl Responder {
     HttpResponse::Ok().content_type("text/html").body(
         tmpl.render("index.html", &tera::Context::new()).unwrap()
     )
 }
 
-#[post("/")]
-async fn index_post(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
+
+#[get("/login")]
+async fn login_form(
+    tmpl: web::Data<tera::Tera>
+) -> impl Responder {
+    HttpResponse::Ok().content_type("text/html").body(
+        tmpl.render("login.html", &tera::Context::new()).unwrap()
+    )
+}
+
+
+#[derive(Deserialize)]
+struct LoginData {
+    username: String,
+    password: String,
+}
+
+#[post("/login")]
+async fn login_verify(
+    tmpl: web::Data<tera::Tera>,
+    login_data: web::Form<LoginData>
+) -> impl Responder {
+    if is_login_data_valid(&login_data) {
+        HttpResponse::Found().header(http::header::LOCATION, "/").finish()
+    } else {
+        let mut context = tera::Context::new();
+        context.insert("error", "Invalid credentials!");
+        context.insert("username", &login_data.username);
+        HttpResponse::Unauthorized().content_type("text/html").body(
+            tmpl.render("login.html", &context).unwrap()
+        )
+    }
 }
 
 
@@ -29,10 +62,15 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .data(tera)
             .service(index)
-            .service(index_post)
+            .service(login_form)
+            .service(login_verify)
 
     })
     .bind("127.0.0.1:8080")?
     .run()
     .await
+}
+
+fn is_login_data_valid(login_data: &web::Form<LoginData>) -> bool {
+    login_data.username == "jlk"
 }
